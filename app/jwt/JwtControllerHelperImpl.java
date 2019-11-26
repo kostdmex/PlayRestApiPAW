c13af2036da53e68a9389a98d65cb8b82abb4f8a
bartlomiej.wloczyk <bartlomiej.wloczyk@assecods.pl>
2019-11-26 13:46:48 +0100

    fix

--- app/jwt/JwtControllerHelperImpl.java
+++ app/jwt/JwtControllerHelperImpl.java
@@ -26,7 +26,7 @@ public class JwtControllerHelperImpl implements JwtControllerHelper {
     public Result verify(Http.Request request, Function<F.Either<JwtValidator.Error, VerifiedJwt>, Result> f) {
         System.out.println("Headers");
         System.out.println(request.getHeaders());
-        request.getHeaders(request.getHeaders().get(HEADER_AUTHORIZATION))
+		System.out.println(request.getHeaders().get(HEADER_AUTHORIZATION));
         Optional<String> authHeader =  request.getHeaders().get(HEADER_AUTHORIZATION);
 
         if (!authHeader.filter(ah -> ah.contains(BEARER)).isPresent()) {
